c3302dff2871db01e974785de424a8f0f54f76fb
julien <julien.genestoux@gmail.com>
2014-11-08 13:58:46 +0100

    fixing affix

--- contents/assets/documentation.js
+++ contents/assets/documentation.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+	var initialTop = $('.docs-inline-nav').position().top;
+	$(document).on('scroll', function() {
+		if($(document).scrollTop() > initialTop) {
+			$('.docs-inline-nav').css({ top: '0' });
+			$('.docs-inline-nav').addClass('docs-inline-nav-fixed');
+		}
+		else {
+			$('.docs-inline-nav').removeClass('docs-inline-nav-fixed');
+		}
+  });
 });
