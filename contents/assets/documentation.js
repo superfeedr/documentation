bca0e25a544e08f389da739c6b199e52ce3083b9
julien <julien.genestoux@gmail.com>
2014-11-08 14:17:17 +0100

    missing scrollspy and affix

--- contents/assets/documentation.js
+++ contents/assets/documentation.js
@@ -1,12 +1,2 @@
-$(document).ready(function(){
-	var initialTop = $('.docs-inline-nav').position().top;
-	$(document).on('scroll', function() {
-		if($(document).scrollTop() > initialTop) {
-			$('.docs-inline-nav').css({ top: '0' });
-			$('.docs-inline-nav').addClass('docs-inline-nav-fixed');
-		}
-		else {
-			$('.docs-inline-nav').removeClass('docs-inline-nav-fixed');
-		}
-  });
+$(document).ready(function() {
 });
